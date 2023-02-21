7952bdc6a61b20ef5fca7142025c67c1c4416394
Carter Smith <97984121+CarterGSmith@users.noreply.github.com>
2023-02-21 14:02:49 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -15,7 +15,8 @@ package midtermexam_versiona_extensioncode;
  */
 public class UserProfile 
 {
-    
+    // COMMENTS
+    // COMMENTS
     private String userID;//the userID
     private String genre;// the user's preferred genre of movie
     private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
